ebbbea547ceaf3e770942b4009ddbd96c79ce9ab
mimi0000oo <mimiionescu07@gmail.com>
2025-07-08 22:03:12 +0300

    V0.0.2 - Added backend with socket.io support and docker integration for deployment

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import { Server } from "socket.io";
+import { Database } from "bun:sqlite";
+
+const db = new Database("db.sqlite");
+
+const PORT = 3000;
+const io = new Server(PORT, { 
+  cors: {
+    origin: "*"
+  }
+ });
+console.log(`Started server on port ${PORT}!`);
+
+io.on("connection", (socket) => {
+  console.log("Hello socket.io", socket.id)
+});
