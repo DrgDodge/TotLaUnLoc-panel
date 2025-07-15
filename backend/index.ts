872796b11bb1f7efe5b3ab5eed37cf8e9d10bec5
mimi0000oo <mimiionescu07@gmail.com>
2025-07-15 22:08:50 +0300

    V0.3.1 - Added pocketbase database, login with auth with backend,  added socketio to both frontend and client

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,58 @@
-import { Server } from "socket.io";
-import { Database } from "bun:sqlite";
+import { Server, Socket } from "socket.io";
+import PocketBase from 'pocketbase';
 
-const db = new Database("db.sqlite", {create: true});
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+await pb.collection("_superusers").authWithPassword("mimiionescu07@gmail.com", "Khgprksz2YugCjB")
+
+let superusers = await pb.collection("_superusers").getList().then(x => {
+  return x.items.map(y => y = y.email);
+})
+console.log(superusers)
 
 const PORT = 7355;
-const io = new Server(PORT, { 
+const io = new Server(PORT, {
   cors: {
     origin: "*"
   }
- });
+});
 console.log(`Started server on port ${PORT}!`);
 
 io.on("connection", (socket) => {
-  console.log("Hello socket.io", socket.id)
+
+  socket.on("auth", async (data, callback) => {
+
+    console.log(data)
+
+    let userAuth = {
+      authentificated: false,
+      isSuperUser: false
+    }
+
+    if (superusers.find(email => email == data.username)) {
+
+      const authData = await pb.collection("_superusers").authWithPassword(data.username, data.password)
+        .then(() => { return true })
+        .catch(() => { return false })
+
+      if (!authData) return callback(userAuth);
+
+      userAuth.authentificated = true
+      userAuth.isSuperUser = true
+
+      return callback(userAuth);
+    }
+
+    const authData = await pb.collection("users").authWithPassword(data.username, data.password)
+      .then(() => { return true })
+      .catch(() => { return false })
+
+    if (!authData) return callback(userAuth);
+
+    userAuth.authentificated = true
+
+    return callback(userAuth);
+
+  })
+
 });
