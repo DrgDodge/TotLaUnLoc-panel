2744f4b295483c50b4f38492d774e07928da7dfe
DrgDodge <dragonul530@gmail.com>
2025-07-23 23:26:14 +0300

    V0.6.3 - Fix url on socketio

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,7 +2,7 @@
   import { goto } from "$app/navigation";
   import { io } from "socket.io-client";
 
-  const socket = io({
+  const socket = io("https://api.totlaunloc.top", {
     withCredentials: true,
   });
 
