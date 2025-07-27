4e76c9e6f271fde7a9c32efbfc82f451482574c7
mimi0000oo <mimiionescu07@gmail.com>
2025-07-27 23:03:00 +0300

    V0.8.6 - Migrate to the new auth system

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,13 +1,5 @@
-// import type { LayoutServerLoad } from './$types';
-
 import { pb } from "$lib/utils";
 import type { LayoutLoad } from "./$types";
-
-// export const load: LayoutServerLoad = async ({ locals }) => {
-//   return {
-//     isSuperUser: locals.user?.isSuperUser || false,
-//   };
-// };
 
 export const load: LayoutLoad = async () => {
   await pb.collection("users").authRefresh().catch(() => { /*user not logged*/ })
--- frontend/src/routes/logout/+server.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export async function POST({ cookies }) {
-  cookies.set('isLoggedIn', '', { path: '/', expires: new Date(0) });
-  cookies.set('isSuperUser', '', { path: '/', expires: new Date(0) });
-  return json({ success: true });
-}
